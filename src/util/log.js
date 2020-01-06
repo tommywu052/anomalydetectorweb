768222c9d020ca5738969c7b10da2f20510fdd97
tommywu052 <tommywu052@gmail.com>
2020-01-06 17:55:25 +0800

    Add files via upload

---
+++ src/util/log.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export class Logger {
+  info(msg) {
+    console.info(msg);
+  }
+
+  warn(msg) {
+    console.warn(msg);
+  }
+
+  error(msg) {
+    console.error(msg);
+  }
+}
